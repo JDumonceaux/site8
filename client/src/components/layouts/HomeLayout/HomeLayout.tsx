000ca514db14ffd166700927dd35bcd87ca0c8e4
john <jdumonceaux@gmail.com>
2024-07-19 23:52:33 -0500

    Revise Homelayout

--- client/src/components/layouts/HomeLayout/HomeLayout.tsx
+++ client/src/components/layouts/HomeLayout/HomeLayout.tsx
@@ -3,14 +3,12 @@ import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
 
-export const HomeLayout = (): JSX.Element => (
+const HomeLayout = (): JSX.Element => (
   <ErrorBoundary fallback={<div>Something went wrong</div>}>
     <AppSetup />
-    <div>
-      <Suspense fallback="Loading ...">
-        <Outlet />
-      </Suspense>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Outlet />
+    </Suspense>
   </ErrorBoundary>
 );
 
