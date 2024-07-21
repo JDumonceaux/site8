0d7e961209489b6499907018438b729f130f7135
JRD <jdumonceaux@gmail.com>
2024-07-21 12:47:01 -0500

    Package updates

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,24 +5,20 @@ import './utils/i18';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-
-import { ReduxProvider } from './providers/ReduxProvider';
 import { RouterProvider } from './providers/RouterProvider';
 import reportWebVitals from './utils/reportWebVitals';
+import ReduxProvider from 'providers/ReduxProvider';
 // import { Amplify } from 'aws-amplify';
 // import config from './amplifyconfiguration.json';
-import { ApolloProvider } from './providers/ApolloProvider';
 // Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider>
-      <ReduxProvider>
-        <HelmetProvider>
-          <RouterProvider />
-        </HelmetProvider>
-      </ReduxProvider>
-    </ApolloProvider>
+    <ReduxProvider>
+      <HelmetProvider>
+        <RouterProvider />
+      </HelmetProvider>
+    </ReduxProvider>
   </React.StrictMode>,
 );
 
