0e1ff0992494b5aca5d138339e5e4320fc5273da
JRD <jdumonceaux@gmail.com>
2024-07-24 13:00:04 -0500

    Add env.ts

---
+++ server1/src/lib/env.ts
@@ -0,0 +1,13 @@
+import * as z from 'zod';
+
+const envSchema = z.object({
+  BASE_URL: z.string().url(),
+  PORT: z.number(),
+  USE_AUTH: z.string().transform((value) => value === 'true'),
+});
+
+export const env = envSchema.parse({
+  // BASE_URL: import.meta.env.VITE_BASE_URL,
+  // PORT: import.meta.env.VITE_PORT,
+  // USE_AUTH: import.meta.env.VITE_USE_AUTH,
+});
