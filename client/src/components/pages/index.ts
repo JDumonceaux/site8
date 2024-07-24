b8063f1fc999c0b8929add029b6c33b23c33f8f5
JRD <jdumonceaux@gmail.com>
2024-07-23 22:39:37 -0500

    Add indexes

---
+++ client/src/components/pages/index.ts
@@ -0,0 +1,6 @@
+export * from './pages/BooksPage/BooksPage';
+export * from './pages/GenericPage/GenericPage';
+export * from './pages/HomePage/HomePage';
+export * from './pages/ImageEditPage/ImageEditPage';
+export * from './pages/ImagesEditPage/ImagesEditPage';
+export * from './pags/MusicPage/MusicPage';
