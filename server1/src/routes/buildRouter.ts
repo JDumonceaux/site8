c8c3930a7295545cccda429648f1d78f115dd723
JRD <jdumonceaux@gmail.com>
2025-01-01 14:47:52 -0600

    Fix path

--- server1/src/routes/buildRouter.ts
+++ server1/src/routes/buildRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { buildFeature } from 'src/features/build/buildFeature.js';
+import { buildFeature } from '../features/build/buildFeature.js';
 
 export const buildRouter = express.Router();
 
