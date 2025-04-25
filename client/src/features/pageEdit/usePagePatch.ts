437ec19e363f8e0f7b6d0ddf1fc23c1e5c78106f
JRD <jdumonceaux@gmail.com>
2025-04-25 13:56:44 -0500

    validation

--- client/src/features/pageEdit/usePagePatch.ts
+++ client/src/features/pageEdit/usePagePatch.ts
@@ -2,7 +2,8 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import useSnackbar from 'features/app/useSnackbar';
 import { ServiceUrl } from 'lib/utils/constants';
-import type { Page, PageEdit } from 'types';
+import { type Page, type PageEdit } from 'types';
+import { z } from 'zod';
 
 export type FormState = {
   fieldData: Page;
@@ -36,12 +37,44 @@ const usePagePatch = () => {
     },
   });
 
+  const isValid = (data: PageEdit): boolean => {
+    const schema = z.object({
+      id: z.number(),
+      name: z
+        .string({
+          invalid_type_error: 'Name must be a string',
+          required_error: 'Name is required.',
+        })
+        .min(1, 'Name is required.')
+        .max(500, 'Name max length exceeded: 500')
+        .trim(),
+      parent: z.string().min(1, 'Parent is required.'),
+      readability_score: z.string().trim().optional(),
+      reading_time: z.string().trim().optional(),
+      text: z.string().trim(),
+      to: z.string().trim().optional(),
+      url: z.string().trim().optional(),
+    });
+    if (schema.safeParse(data)) {
+      return true;
+    }
+    setMessage('Validation error: Invalid data');
+    return false;
+  };
+
   const patchItem = async (
     _prevState: unknown,
     formData: FormData,
   ): Promise<FormState> => {
     const temp = Object.fromEntries(formData.entries());
     const data: PageEdit = { ...temp, id: Number(temp.id) } as PageEdit;
+
+    if (!isValid(data)) {
+      return {
+        fieldData: {},
+        message: 'Validation error: Invalid data',
+      } as FormState;
+    }
 
     try {
       await mutation.mutateAsync(data);
